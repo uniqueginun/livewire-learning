2326711eb0081030259bd14f9bad79b57abb159b
Othman Mohammed <uniqueginun@gmail.com>
2020-12-30 00:09:08 +0300

    add title slot

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -10,7 +10,7 @@
 
     <livewire:styles />
 
-    <title>Livewire learning</title>
+    <title>Livewire learning {{ $slot('title') }} </title>
 </head>
 <body>
 
@@ -19,7 +19,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    
+
     <livewire:scripts />
 </body>
 </html>
